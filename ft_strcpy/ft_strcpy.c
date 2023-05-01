2fa68d0d0cd43683d21ef73f2764a138bfe7d1f1
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-01 15:29:03 +0930

    Level 0

--- ft_strcpy/ft_strcpy.c
+++ ft_strcpy/ft_strcpy.c
@@ -5,19 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/27 15:37:08 by btomlins          #+#    #+#             */
-/*   Updated: 2023/04/27 15:43:26 by btomlins         ###   ########.fr       */
+/*   Created: 2023/05/01 14:58:51 by btomlins          #+#    #+#             */
+/*   Updated: 2023/05/01 15:23:34 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+/*
+Assignment name  : ft_strcpy
+Expected files   : ft_strcpy.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Reproduce the behavior of the function strcpy (man strcpy).
+
+Your function must be declared as follows:
+
+char    *ft_strcpy(char *s1, char *s2);
+*/
+
 #include <unistd.h>
 
 char	*ft_strcpy(char *s1, char *s2)
 {
-	int	i;
+	int i = 0;
 
-	i = 0;
 	while (s2[i] != '\0')
 	{
 		s1[i] = s2[i];
