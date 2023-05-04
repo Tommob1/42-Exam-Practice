51a4bd54165fda4d2644d8183a4023af764ae8c6
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-04 16:29:27 +0930

    Level 2

---
+++ str_capitalizer/str_capitalizer.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   str_capitalizer.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 15:40:06 by btomlins          #+#    #+#             */
+/*   Updated: 2023/05/04 16:27:03 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : str_capitalizer
+Expected files   : str_capitalizer.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes one or several strings and, for each argument,
+capitalizes the first character of each word (If it's a letter, obviously),
+puts the rest in lowercase, and displays the result on the standard output,
+followed by a \n.
+
+A "word" is defined as a part of a string delimited either by spaces/tabs, or
+by the start/end of the string. If a word only has one letter, it must be
+capitalized.
+
+If there are no arguments, the progam must display \n.
+*/
+
+#include <unistd.h>
+
+void	str_capitalizer(char *str)
+{
+	int i = 0;
+
+	if (str[i] >= 'a' && 'z' >= str[i])
+			str[i] -= 32;
+	write(1, &str[i], 1);
+	while (str[++i])
+	{
+		if (str[i] >= 'A' && 'Z' >= str[i])
+				str[i] += 32;
+		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' ||
+		str[i - 1] == '\t'))
+				str[i] -= 32;
+		write(1, &str[i], 1);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int i;
+	
+	if (argc < 2)
+		write(1, "\n", 1);
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			str_capitalizer(argv[i]);
+			write(1, "\n", 1);
+			i += 1;
+		}
+	}
+	return (0);
+}
