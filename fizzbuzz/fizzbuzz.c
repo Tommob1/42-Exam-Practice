e053f0c9edf588d3ebdb406ce8b401e7d3a4c399
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-05-16 15:59:51 +0930

    Level 0

---
+++ fizzbuzz/fizzbuzz.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 15:49:41 by btomlins          #+#    #+#             */
+/*   Updated: 2023/05/16 15:57:49 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : fizzbuzz
+Expected files   : fizzbuzz.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that prints the numbers from 1 to 100, each separated by a
+newline.
+
+If the number is a multiple of 3, it prints 'fizz' instead.
+
+If the number is a multiple of 5, it prints 'buzz' instead.
+
+If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
+*/
+
+#include <unistd.h>
+
+void	ft_write_number(int number)
+{
+	char	str[10] = "0123456789";
+
+	if (number > 9)
+		ft_write_number(number / 10);
+	write(1, &str[number % 10], 1);
+}
+
+int	main()
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else if (i % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_write_number(i);
+		i++;
+		write(1, "\n", 1);
+	}
+
+}
