0e61dd81847ff641b2ebf3aabb4a5a51ca134a50
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-06-05 13:22:34 +0930

    Level 1

---
+++ print_bits (3)/print_bits.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_bits.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 13:19:20 by btomlins          #+#    #+#             */
+/*   Updated: 2023/06/05 13:22:09 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : print_bits
+Expected files   : print_bits.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a function that takes a byte, and prints it in binary WITHOUT A NEWLINE
+AT THE END.
+
+Your function must be declared as follows:
+
+void	print_bits(unsigned char octet);
+
+Example, if you pass 2 to print_bits, it will print "00000010"%
+*/
+
+#include <unistd.h>
+
+void	print_bits(unsigned char octet)
+{
+	int	i;
+	unsigned char	bit;
+
+	i = 8;
+
+	while (i--)
+	{
+		bit = (octet >> i & 1) + '0';
+		write(1, &bit, 1);
+	}
+}
