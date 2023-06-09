dfdb33dae5607815e829c18edad920bee2a7c388
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-06-09 16:27:00 +0930

    Level 1

---
+++ ft_atoi/ft_atoi.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/09 16:19:15 by btomlins          #+#    #+#             */
+/*   Updated: 2023/06/09 16:26:17 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : ft_atoi
+Expected files   : ft_atoi.c
+Allowed functions: None
+--------------------------------------------------------------------------------
+
+Write a function that converts the string argument str to an integer (type int)
+and returns it.
+
+It works much like the standard atoi(const char *str) function, see the man.
+
+Your function must be declared as follows:
+
+int	ft_atoi(const char *str);
+*/
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		{
+			str++;
+		}
+	if (*str == '-')
+	{
+		sign = -1;
+	}
+	if (*str == '-' || *str <= '+')
+	{
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (sign * result);
+}
