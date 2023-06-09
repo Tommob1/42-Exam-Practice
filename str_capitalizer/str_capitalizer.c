1217e090b7789691298befb068e6b9513097fa54
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-06-09 16:37:17 +0930

    Level 2

--- str_capitalizer/str_capitalizer.c
+++ str_capitalizer/str_capitalizer.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/04 15:40:06 by btomlins          #+#    #+#             */
-/*   Updated: 2023/05/04 16:27:03 by btomlins         ###   ########.fr       */
+/*   Created: 2023/06/09 16:27:52 by btomlins          #+#    #+#             */
+/*   Updated: 2023/06/09 16:36:32 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,36 @@ by the start/end of the string. If a word only has one letter, it must be
 capitalized.
 
 If there are no arguments, the progam must display \n.
+
+Example:
+
+$> ./str_capitalizer | cat -e
+$
+$> ./str_capitalizer "a FiRSt LiTTlE TESt" | cat -e
+A First Little Test$
+$> ./str_capitalizer "__SecONd teST A LITtle BiT   Moar comPLEX" "   But... This iS not THAT COMPLEX" "     Okay, this is the last 1239809147801 but not    the least    t" | cat -e
+__second Test A Little Bit   Moar Complex$
+   But... This Is Not That Complex$
+     Okay, This Is The Last 1239809147801 But Not    The Least    T$
 */
 
 #include <unistd.h>
 
 void	str_capitalizer(char *str)
 {
-	int i = 0;
+	int i;
+	i = 0;
 
-	if (str[i] >= 'a' && 'z' >= str[i])
-			str[i] -= 32;
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
 	write(1, &str[i], 1);
+
 	while (str[++i])
 	{
-		if (str[i] >= 'A' && 'Z' >= str[i])
-				str[i] += 32;
-		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' ||
-		str[i - 1] == '\t'))
-				str[i] -= 32;
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		if ((str[i] >= 'a' && str[i] <='z') && (str[i - 1] == ' ' || str[i - 1] == '\t'))
+			str[i] -= 32;
 		write(1, &str[i], 1);
 	}
 }
@@ -51,6 +63,7 @@ void	str_capitalizer(char *str)
 int	main(int argc, char *argv[])
 {
 	int i;
+	i = 0;
 	
 	if (argc < 2)
 		write(1, "\n", 1);
